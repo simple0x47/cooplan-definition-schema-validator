7041d3ff2dd317ca7df64d7ab4e5dc9a3e68e31a
Gabriel Ami <gabriel@simpleg.eu>
2022-11-20 12:30:07 +0200

    + bundle only definition's version

--- src/definition_value.rs
+++ src/definition_value.rs
@@ -4,19 +4,19 @@ use serde_json::Value;
 /// Validated, definition scoped, JSON value.
 #[derive(Debug)]
 pub struct DefinitionValue {
-    definition: Definition,
+    definition: String,
     value: Value,
 }
 
 impl DefinitionValue {
     pub fn new(definition: &Definition, value: Value) -> DefinitionValue {
         DefinitionValue {
-            definition: definition.clone(),
+            definition: definition.version(),
             value,
         }
     }
 
-    pub fn definition(&self) -> &Definition {
+    pub fn definition(&self) -> &String {
         return &self.definition;
     }
 
