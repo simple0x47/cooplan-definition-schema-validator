9413be8e85fea362638354b70dec2583cd11b090
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-01-30 21:07:06 +0200

    + JSON object validation

--- src/schema_validator.rs
+++ src/schema_validator.rs
@@ -118,6 +118,80 @@ impl SchemaValidator {
         }
     }
 
+    pub fn validate_object(
+        &mut self,
+        mut object: Map<String, Value>,
+        definition: Definition,
+    ) -> Result<DefinitionValue, Error> {
+        let value_definition_version = match self.try_get_version(&object) {
+            Ok(version) => version,
+            Err(error) => return Err(error),
+        };
+
+        if definition.version().to_lowercase() != value_definition_version.to_lowercase() {
+            return Err(Error::new(
+                ErrorKind::ValueDefinitionMismatch,
+                format!(
+                    "value's version '{}' != definition's version '{}'",
+                    value_definition_version,
+                    definition.version()
+                ),
+            ));
+        }
+
+        let value_type = match self.try_get_type(&object) {
+            Ok(value_type) => value_type,
+            Err(error) => return Err(error),
+        };
+
+        let attributes: Vec<ValidatedSourceAttribute> =
+            match self.try_get_attributes_from_definition(&definition, &value_type) {
+                Ok(attributes) => attributes,
+                Err(error) => return Err(error),
+            };
+
+        let mut scoped_value: Map<String, Value> = Map::new();
+
+        for attribute in attributes.as_slice() {
+            let attribute_value = match object.remove(&attribute.id) {
+                Some(attribute_value) => attribute_value,
+                None => {
+                    return Err(Error::new(
+                        ErrorKind::InvalidValue,
+                        format!(
+                            "failed to find attribute id '{}' within value",
+                            attribute.id
+                        ),
+                    ))
+                }
+            };
+
+            match self.validations.get(&attribute.data_type) {
+                Some(validation) => match validation(&attribute_value) {
+                    Ok(_) => {
+                        scoped_value.insert(attribute.id.clone(), attribute_value);
+                    }
+                    Err(error) => return Err(error),
+                },
+                None => {
+                    return Err(Error::new(
+                        ErrorKind::ValidationNotRegistered,
+                        format!(
+                            "no validation found for data type '{}'",
+                            attribute.data_type
+                        ),
+                    ))
+                }
+            }
+        }
+
+        let category_chain = build_from_definition_and_category(&definition, &value_type);
+
+        let definition_value = DefinitionValue::try_new(&definition, category_chain, scoped_value)?;
+
+        Ok(definition_value)
+    }
+
     fn try_get_version(&self, object: &Map<String, Value>) -> Result<String, Error> {
         match object.get(VALUE_VERSION) {
             Some(version) => match version.as_str() {
